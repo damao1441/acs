8dcde2b3d6f1c9f153a8ab9d46d09658bfa156fe
Irina <irina.epshteyn1@ge.com>
2017-01-23 09:29:24 -0800

    Renamed and improved integration test for DSE graph.

--- acs-integration-tests/src/test/java/com/ge/predix/integration/test/DSEGraphIT.java
+++ acs-integration-tests/src/test/java/com/ge/predix/integration/test/DSEGraphIT.java
@@ -14,53 +14,66 @@ import org.testng.annotations.Test;
 @ContextConfiguration("classpath:integration-test-spring-context.xml")
 @Test
 public class DSEGraphIT extends AbstractTestNGSpringContextTests {
-    public void testDSEGraph() throws Exception {
-        Cluster cluster = Cluster.build().addContactPoint("127.0.0.1").create();
-        System.out.println("testDSEGraph(): cluster = " + cluster);
+    public void testWithTinkerPopGremlinDriver() throws Exception {
+        Cluster cluster = null;
+        Client client = null;
+        try {
+            cluster = Cluster.build().addContactPoint("127.0.0.1").create();
+            System.out.println("testDSEGraph(): cluster = " + cluster);
 
-        Client client = cluster.connect();
-        System.out.println("testDSEGraph(): client = " + client.toString());
+            client = cluster.connect();
+            System.out.println("testDSEGraph(): client = " + client.toString());
 
-        // 1. Any exceptions executing this command are propagated to the client in the ResultSet.
-        // For example, setting graph name to "my-graph" produces the following exception in gremlin server:
-        // IllegalArgumentException:
-        // gremlin.graph.name must must only consist of letters, numbers, and underscores: my-graph is invalid
-        //
-        // 2. Keyspace name = graph name
-        //
-        // 3. Running the test second time produces IllegalArgumentException: Graph mygraph already exists.
-        CompletableFuture<List<Result>> results = client.submit("system.graph('mygraph').ifNotExists().create()").all();
-        System.out.println("testDSEGraph(): create graph results = " + results.get().toString());
+            // 1. Any exceptions executing this command are propagated to the client in the ResultSet.
+            // For example, setting graph name to "my-graph" produces the following exception in gremlin server:
+            // IllegalArgumentException:
+            // gremlin.graph.name must must only consist of letters, numbers, and underscores: my-graph is invalid
+            //
+            // 2. Keyspace name = graph name
+            //
+            // 3. Running the test second time produces IllegalArgumentException: Graph mygraph already exists.
+            CompletableFuture<List<Result>> results = client.submit("system.graph('mygraph').ifNotExists().create()").all();
+            System.out.println("testDSEGraph(): create graph results = " + results.get().toString());
 
-        Client aliasClient = client.alias("mygraph.g");
-        results = aliasClient.submit("g").all();
-        System.out.println("testDSEGraph(): alias results = " + results.get().toString());
-        Assert.assertEquals(results.get().get(0).getString(), "graphtraversalsource[dsegraphimpl[mygraph], standard]");
+            Client aliasClient = null;
+            try {
+                aliasClient = client.alias("mygraph.g");
+                results = aliasClient.submit("g").all();
+                System.out.println("testDSEGraph(): alias results = " + results.get().toString());
+                Assert.assertEquals(results.get().get(0).getString(), "graphtraversalsource[dsegraphimpl[mygraph], standard]");
 
-        results = aliasClient.submit("g.V().count()").all();
-        System.out.println("testDSEGraph(): V() count results = " + results.get().toString());
-        Assert.assertEquals(0, results.get().get(0).getLong());
+                results = aliasClient.submit("g.V().count()").all();
+                System.out.println("testDSEGraph(): V() count results = " + results.get().toString());
+                Assert.assertEquals(0, results.get().get(0).getLong());
 
-        results = aliasClient.submit("g.addV('author').property('name','Julia Child')").all();
-        System.out.println("testDSEGraph(): addV('author') results = " + results.get().toString());
+                results = aliasClient.submit("g.addV('author').property('name','Julia Child')").all();
+                System.out.println("testDSEGraph(): addV('author') results = " + results.get().toString());
 
-        results = aliasClient
-                .submit("g.addV('book').property('name', 'The French Chef Cookbook').property('year' , 1968)").all();
-        System.out.println("testDSEGraph(): addV('book') results = " + results.get().toString());
+                results = aliasClient
+                        .submit("g.addV('book').property('name', 'The French Chef Cookbook').property('year' , 1968)").all();
+                System.out.println("testDSEGraph(): addV('book') results = " + results.get().toString());
 
-        results = aliasClient.submit("g.V().count()").all();
-        System.out.println("testDSEGraph(): V() count results = " + results.get().toString());
-        Assert.assertEquals(2, results.get().get(0).getLong());
+                results = aliasClient.submit("g.V().count()").all();
+                System.out.println("testDSEGraph(): V() count results = " + results.get().toString());
+                Assert.assertEquals(2, results.get().get(0).getLong());
+            } finally {
+                // System commands cannot be executed while alias is set.
+                // There is no explicit to clear aliases but closing the client
+                if (aliasClient != null) {
+                    aliasClient.close();
+                }
+            }
 
-        // System commands cannot be executed while alias is set.
-        // There is no explicit to clear aliases but closing the client
-        aliasClient.close();
-
-        // Will fails if aliases are not cleared.
-        results = client.submit("system.graph('mygraph').drop()").all();
-        System.out.println("testDSEGraph(): remove graph results = " + results.get().toString());
-
-        client.close();
-        cluster.close();
+            // Will fails if aliases are not cleared.
+            results = client.submit("system.graph('mygraph').drop()").all();
+            System.out.println("testDSEGraph(): remove graph results = " + results.get().toString());
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+            if (cluster != null) {
+                cluster.close();
+            }
+        }
     }
 }
