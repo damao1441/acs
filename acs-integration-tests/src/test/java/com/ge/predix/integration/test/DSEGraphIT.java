672e1747ad59623b4e6a637fc84f046f9720f019
Irina <irina.epshteyn1@ge.com>
2017-01-30 18:16:20 -0800

    Added schema for custom vertex ids.

--- acs-integration-tests/src/test/java/com/ge/predix/integration/test/DSEGraphIT.java
+++ acs-integration-tests/src/test/java/com/ge/predix/integration/test/DSEGraphIT.java
@@ -1,5 +1,9 @@
 package com.ge.predix.integration.test;
 
+import static com.ge.predix.acs.privilege.management.dao.GraphGenericRepository.ZONE_ID_KEY;
+import static com.ge.predix.acs.privilege.management.dao.GraphResourceRepository.RESOURCE_ID_KEY;
+import static com.ge.predix.acs.privilege.management.dao.GraphResourceRepository.RESOURCE_LABEL;
+
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -11,6 +15,7 @@ import org.apache.tinkerpop.gremlin.driver.Result;
 import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.structure.util.GraphFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -20,7 +25,25 @@ import org.testng.annotations.Test;
 @ContextConfiguration("classpath:integration-test-spring-context.xml")
 @Test
 public class DSEGraphIT extends AbstractTestNGSpringContextTests {
+    static final String SYSTEM_CREATE_GRAPH = "system.graph('%s').ifNotExists().create()";
+    static final String SYSTEM_DROP_GRAPH = "system.graph('%s').drop()";
+
+    static final String SCHEMA_CREATE_PROPERTY_KEY = "schema.propertyKey('%s').Text().ifNotExists().create()";
+    static final String SCHEMA_CREATE_CUSTOM_VERTEX_ID = "schema.vertexLabel('%s').partitionKey('%s')"
+            + ".clusteringKey('%s').ifNotExists().create()";
+    static final String SCHEMA_DESCRIBE = "schema.describe()";
+    static final String SCHEMA_CLEAR = "schema.clear()";
+
+    static final String QUERY_VERTEX_COUNT = "g.V().count()";
+    static final String QUERY_ADD_VERTEX = "g.addV('%s').property('%s','%s').property('%s','%s')";
+    static final String QUERY_VERTEX = "g.V(['~label':'%s','%s':'%s','%s':'%s'])";
+
     public void testWithTinkerPopGremlinDriver() throws Exception {
+        final String graphName = "mygraph";
+        final String zoneIdKey = ZONE_ID_KEY;
+        final String resourceIdKey = RESOURCE_ID_KEY;
+        final String resourceLabel = RESOURCE_LABEL;
+        
         Cluster cluster = null;
         Client client = null;
         try {
@@ -30,54 +53,98 @@ public class DSEGraphIT extends AbstractTestNGSpringContextTests {
             client = cluster.connect();
             System.out.println("testWithTinkerPopGremlinDriver(): client = " + client.toString());
 
-            // 1. Any exceptions executing this command are propagated to the client in the ResultSet.
-            // For example, setting graph name to "my-graph" produces the following exception in gremlin server:
-            // IllegalArgumentException:
-            // gremlin.graph.name must must only consist of letters, numbers, and underscores: my-graph is invalid
-            //
-            // 2. Keyspace name = graph name
-            //
-            // 3. Running the test second time produces IllegalArgumentException: Graph mygraph already exists.
-            CompletableFuture<List<Result>> results = client.submit("system.graph('mygraph').ifNotExists().create()")
+            // Create an empty DSE graph
+            // Cassandra keyspace name is equal to graph name
+            CompletableFuture<List<Result>> results = client.submit(String.format(SYSTEM_CREATE_GRAPH, graphName))
                     .all();
             System.out.println("testWithTinkerPopGremlinDriver(): create graph results = " + results.get().toString());
 
             Client aliasClient = null;
             try {
-                aliasClient = client.alias("mygraph.g");
+                aliasClient = client.alias(graphName + ".g");
                 results = aliasClient.submit("g").all();
-                System.out.println("testWithTinkerPopGremlinDriver(): alias results = " + results.get().toString());
+                System.out.println("testWithTinkerPopGremlinDriver(): traversal source = " + results.get().toString());
                 Assert.assertEquals(results.get().get(0).getString(),
-                        "graphtraversalsource[dsegraphimpl[mygraph], standard]");
+                        "graphtraversalsource[dsegraphimpl[" + graphName +"], standard]");
 
-                results = aliasClient.submit("g.V().count()").all();
-                System.out.println("testWithTinkerPopGremlinDriver(): V() count results = " + results.get().toString());
+                // Create graph schema
+                // Schema should be created after alias is set
+                results = aliasClient.submit(String.format(SCHEMA_CREATE_PROPERTY_KEY, zoneIdKey)).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): create zone id property key result = "
+                        + results.get().toString());
+                results = aliasClient.submit(String.format(SCHEMA_CREATE_PROPERTY_KEY, resourceIdKey)).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): create resource id property key result = "
+                        + results.get().toString());
+                results = aliasClient
+                        .submit(String.format(SCHEMA_CREATE_CUSTOM_VERTEX_ID, resourceLabel, zoneIdKey, resourceIdKey))
+                        .all();
+                System.out.println("testWithTinkerPopGremlinDriver(): create custom vertex id result = "
+                        + results.get().toString());
+
+                results = aliasClient.submit(SCHEMA_DESCRIBE).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): resulting schema = " + results.get().toString());
+
+                results = aliasClient.submit(QUERY_VERTEX_COUNT).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): vertices count = " + results.get().toString());
                 Assert.assertEquals(0, results.get().get(0).getLong());
 
-                results = aliasClient.submit("g.addV('author').property('name','Julia Child')").all();
-                System.out.println(
-                        "testWithTinkerPopGremlinDriver(): addV('author') results = " + results.get().toString());
+                // Add vertices
+                results = aliasClient.submit(String.format(QUERY_ADD_VERTEX, resourceLabel, zoneIdKey, "myzone",
+                        resourceIdKey, "resourceOne")).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): add vertex " + results.get().toString());
+//                Assert.assertEquals(results.get().get(0).getString(), String.format("v[{~label=%s, %s=%s, %s=%s}]",
+//                        resourceLabel, zoneIdKey, "myzone", resourceIdKey, "resourceOne"));
 
-                results = aliasClient
-                        .submit("g.addV('book').property('name', 'The French Chef Cookbook').property('year' , 1968)")
-                        .all();
-                System.out.println(
-                        "testWithTinkerPopGremlinDriver(): addV('book') results = " + results.get().toString());
+                results = aliasClient.submit(String.format(QUERY_ADD_VERTEX, resourceLabel, zoneIdKey, "myzone",
+                        resourceIdKey, "resourceTwo")).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): add vertex " + results.get().toString());
+//                Assert.assertEquals(results.get().get(0).getString(), String.format("v[{~label=%s, %s=%s, %s=%s}]",
+//                        resourceLabel, zoneIdKey, "myzone", resourceIdKey, "resourceTwo"));
 
-                results = aliasClient.submit("g.V().count()").all();
-                System.out.println("testWithTinkerPopGremlinDriver(): V() count results = " + results.get().toString());
-                Assert.assertEquals(2, results.get().get(0).getLong());
+                results = aliasClient.submit(String.format(QUERY_ADD_VERTEX, resourceLabel, zoneIdKey, "otherzone",
+                        resourceIdKey, "resourceOne")).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): add vertex " + results.get().toString());
+//                Assert.assertEquals(results.get().get(0).getString(), String.format("v[{~label=%s, %s=%s, %s=%s}]",
+//                        resourceLabel, zoneIdKey, "otherzone", resourceIdKey, "resourceOne"));
+
+                results = aliasClient.submit(String.format(QUERY_ADD_VERTEX, resourceLabel, zoneIdKey, "otherzone",
+                        resourceIdKey, "resourceTwo")).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): add vertex " + results.get().toString());
+//                Assert.assertEquals(results.get().get(0).getString(), String.format("v[{~label=%s, %s=%s, %s=%s}]",
+//                        resourceLabel, zoneIdKey, "otherzone", resourceIdKey, "resourceTwo"));
+
+                results = aliasClient.submit(String.format(QUERY_ADD_VERTEX, resourceLabel, zoneIdKey, "otherzone",
+                        resourceIdKey, "resourceThree")).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): add vertex " + results.get().toString());
+//                Assert.assertEquals(results.get().get(0).getString(), String.format("v[{~label=%s, %s=%s, %s=%s}]",
+//                        resourceLabel, zoneIdKey, "otherzone", resourceIdKey, "resourceThree"));
+
+                results = aliasClient.submit(QUERY_VERTEX_COUNT).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): vertices count = " + results.get().toString());
+                Assert.assertEquals(5, results.get().get(0).getLong());
+
+                results = aliasClient.submit(String.format(QUERY_VERTEX, resourceLabel, zoneIdKey, "otherzone",
+                        resourceIdKey, "resourceOne")).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): get vertex result = " + results.get().toString());
+                Assert.assertEquals(1, results.get().size());
+                Vertex vertex = results.get().get(0).getVertex();
+                Assert.assertEquals(resourceLabel, vertex.label());
+                Assert.assertTrue(vertex.id().toString().contains(zoneIdKey + "=otherzone"));
+                Assert.assertTrue(vertex.id().toString().contains(resourceIdKey + "=resourceOne"));
             } finally {
                 // System commands cannot be executed while alias is set.
                 // There is no explicit to clear aliases but closing the client
                 if (aliasClient != null) {
+                    results = aliasClient.submit(SCHEMA_CLEAR).all();
+                    System.out.println(
+                            "testWithTinkerPopGremlinDriver(): clearing schema results = " + results.get().toString());
                     aliasClient.close();
                 }
-            }
 
-            // Will fails if aliases are not cleared.
-            results = client.submit("system.graph('mygraph').drop()").all();
-            System.out.println("testWithTinkerPopGremlinDriver(): remove graph results = " + results.get().toString());
+                // Will fails if aliases are not cleared.
+                results = client.submit(String.format(SYSTEM_DROP_GRAPH, "mygraph")).all();
+                System.out.println("testWithTinkerPopGremlinDriver(): remove graph results = " + results.get().toString());
+            }
         } finally {
             if (client != null) {
                 client.close();
