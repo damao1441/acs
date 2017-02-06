a7ffdbab322b038be37d242f24515bcc596a94f8
Frank Gasparovic <Frank.Gasparovic@GE.com>
2017-02-06 11:22:59 -0800

    Removed audit dependencies

--- service/src/main/java/com/ge/predix/acs/request/audit/LoggingFilter.java
+++ service/src/main/java/com/ge/predix/acs/request/audit/LoggingFilter.java
@@ -10,9 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import com.ge.predix.audit.AuditClient;
-//import com.ge.predix.audit.AuditCredentials;
+
 
 @Component
 public class LoggingFilter extends OncePerRequestFilter {
@@ -26,13 +24,4 @@ public class LoggingFilter extends OncePerRequestFilter {
         filterChain.doFilter(multiReadRequest, response);
         System.out.println("Status after is: " + response.getStatus());
     }
-    
-//    private AuditClient createAuditClient() {
-//        AuditCredentials credentials = new AuditCredentials(
-//            "ehub.<cf_host>:443", //from VCAP credentials of an application bound to the Audit Service
-//            "<Event-Hub-Zone-Id>", //from VCAP credentials of an application bound to the Audit Service
-//            "<uaa_instance_host>",  //e.g. "https://9d06-2c95a1f4f5ea.predix-uaa.grc-apps.svc.ice.ge.com"
-//            "<client_id>:<client_secret>"); //credentials of auditing OAuth client
-//        return new AuditClient(credentials);
-//    }
 }
